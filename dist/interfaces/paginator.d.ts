3c8442584635d2ec20efbfe6a9762aa3bfcb9b87
guicuton <guilhermesilva22@hotmail.com>
2023-11-28 22:37:42 -0300

    EDIT: interfaces and readme

---
+++ dist/interfaces/paginator.d.ts
@@ -0,0 +1,27 @@
+export interface IPaginationParams {
+    instance: any;
+    entity: string;
+    per_page: number;
+    bottom: number;
+    offset?: number;
+    orderBy: string;
+    orderDirection: 'asc' | 'desc';
+    where?: any;
+    select?: any;
+    include?: any;
+}
+export interface IPaginationMeta {
+    page: number;
+    offset: number;
+    isCurrent: boolean;
+}
+export interface TPaginationData {
+    data: any;
+    meta: {
+        count: number;
+        totalOfPages: number;
+        first: IPaginationMeta;
+        last: IPaginationMeta;
+        around: IPaginationMeta[];
+    };
+}
